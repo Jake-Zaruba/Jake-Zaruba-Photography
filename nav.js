5f0b2ed90951633c2f8c5cb7548a72eeeb15ee0b
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2022-05-01 23:28:49 -0500

    Added/Optimized
    Added mobile navigation and media queries. Also added meta descriptions and titles to optimize SEO.

---
+++ nav.js
@@ -0,0 +1,8 @@
+// MOBILE NAVIGATION //
+
+const btnNav = document.querySelector(`.btn-mobile-nav`);
+const header = document.querySelector(`.header`);
+
+btnNav.addEventListener(`click`, function () {
+  header.classList.toggle(`nav-open`);
+});
