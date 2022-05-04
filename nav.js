52deff94a641bd3bc6daccdde362b31cb5e1415f
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2022-05-03 22:53:50 -0500

    Fixed
    Fixed a bug where the mobile logo would display outside of the mobile navigation. Also fixed the mobile navigation so that it does not appear when the page is refreshed, and disappears when a navigation link is clicked.

--- nav.js
+++ nav.js
@@ -2,7 +2,14 @@
 
 const btnNav = document.querySelector(`.btn-mobile-nav`);
 const header = document.querySelector(`.header`);
+const navLink = document.querySelectorAll(`.nav-link`);
 
 btnNav.addEventListener(`click`, function () {
   header.classList.toggle(`nav-open`);
 });
+
+navLink.forEach((link) =>
+  link.addEventListener(`click`, () => {
+    header.classList.toggle(`nav-open`);
+  })
+);
